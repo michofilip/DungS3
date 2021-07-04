07a6210a702bf59d97e628c3fc8739daade61c75
majki <michofilip@gmail.com>
2021-07-04 20:57:47 +0200

    file names

--- src/main/scala/src/data/file/Resources.scala
+++ src/main/scala/src/data/file/Resources.scala
@@ -6,9 +6,9 @@ import scala.xml.XML
 
 object Resources:
 
-    val physicsFile = File("src/main/resources/physicsEntries.xml")
-    val physicsSelectorsFile = File("src/main/resources/physicsSelectorEntries.xml")
-    val framesFile = File("src/main/resources/frameEntries.xml")
-    val animationsFile = File("src/main/resources/animationEntries.xml")
-    val animationSelectorsFile = File("src/main/resources/animationSelectorEntries.xml")
-    val entityPrototypesFile = File("src/main/resources/entityPrototypeEntries.xml")
+    val physicsFile = File("src/main/resources/physics.xml")
+    val physicsSelectorsFile = File("src/main/resources/physicsSelectors.xml")
+    val framesFile = File("src/main/resources/frames.xml")
+    val animationsFile = File("src/main/resources/animations.xml")
+    val animationSelectorsFile = File("src/main/resources/animationSelectors.xml")
+    val entityPrototypesFile = File("src/main/resources/entityPrototypes.xml")
