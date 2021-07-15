094f45f881575beaa680df50a37e9ed2443eb9bb
majki <michofilip@gmail.com>
2021-07-15 21:40:55 +0200

    EventSerializationService

--- src/main/scala/src/game/service/GameFrameSerializationService.scala
+++ src/main/scala/src/game/service/GameFrameSerializationService.scala
@@ -1,7 +1,6 @@
 package src.game.service
 
 import src.game.entity.EntityRepository
-import src.game.event.EventXmlService
 import src.game.GameFrame
 
 import scala.util.Try
@@ -13,13 +12,13 @@ class GameFrameSerializationService(gameStateSerializationService: GameStateSeri
         <GameFrame>
             {gameStateSerializationService.toXml(gameFrame.gameState)}
             <events>
-                {gameFrame.events.map(EventXmlService.toXml)}
+                {gameFrame.events.map(EventSerializationService.toXml)}
             </events>
         </GameFrame>
 
     def fromXml(xml: Node): Option[GameFrame] = Try {
         val gameState = (xml \ "GameState").headOption.flatMap(gameStateSerializationService.fromXml)
-        val events = (xml \ "events" \ "Event").flatMap(EventXmlService.fromXml)
+        val events = (xml \ "events" \ "Event").flatMap(EventSerializationService.fromXml)
 
         gameState.map { gameState =>
             GameFrame(gameState = gameState, events = events.toVector)
