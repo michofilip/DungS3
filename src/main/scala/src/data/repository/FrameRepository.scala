4936644127622e2abf7f3afc9cf8bfa1a98880e2
majki <michofilip@gmail.com>
2021-08-15 20:03:09 +0200

    FrameRepository redone

--- src/main/scala/src/data/repository/FrameRepository.scala
+++ src/main/scala/src/data/repository/FrameRepository.scala
@@ -4,26 +4,32 @@ import src.data.Resources
 import src.data.model.{FrameEntry, PhysicsEntry}
 import src.game.entity.parts.graphics.Frame
 
+import scala.util.{Failure, Success, Try}
 import scala.xml.XML
 
 final class FrameRepository private(spriteRepository: SpriteRepository) extends Repository[Int, Frame] :
 
     override protected val dataById: Map[Int, Frame] =
-        def convertToFrame(frameEntry: FrameEntry): Option[Frame] =
-            for {
-                sprite <- spriteRepository.findById(frameEntry.spriteId)
-            } yield {
-                Frame(sprite = sprite, offsetX = frameEntry.offsetX, offsetY = frameEntry.offsetY)
+        def mapEntryFrom(frameEntry: FrameEntry): Try[(Int, Frame)] =
+            spriteRepository.findById(frameEntry.spriteId) match {
+                case None => Failure {
+                    new NoSuchElementException(s"Sprite id: ${frameEntry.spriteId} not found!")
+                }
+                case Some(sprite) => Success {
+                    frameEntry.id -> Frame(sprite = sprite, offsetX = frameEntry.offsetX, offsetY = frameEntry.offsetY)
+                }
             }
-
 
         val xml = XML.load(Resources.frames.reader())
 
         (xml \ "Frame")
-            .flatMap(FrameEntry.fromXML)
-            .map(frameEntry => frameEntry.id -> convertToFrame(frameEntry))
-            .collect {
-                case (id, Some(frame)) => id -> frame
+            .map { node =>
+                FrameEntry.fromXML(node)
+                    .flatMap(mapEntryFrom)
+            }
+            .map {
+                case Failure(e) => throw e
+                case Success(mapEntry) => mapEntry
             }.toMap
 
 object FrameRepository:
