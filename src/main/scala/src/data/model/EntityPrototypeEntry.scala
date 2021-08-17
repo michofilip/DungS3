85f8b0c17bfd6649b4b10e3cfe71dbdc36ccf0e0
majki <michofilip@gmail.com>
2021-08-17 19:39:46 +0200

    EntityPrototypeRepository redone

--- src/main/scala/src/data/model/EntityPrototypeEntry.scala
+++ src/main/scala/src/data/model/EntityPrototypeEntry.scala
@@ -1,8 +1,9 @@
 package src.data.model
 
+import src.exception.FailedToReadObject
 import src.game.entity.parts.state.State
 
-import scala.util.Try
+import scala.util.{Failure, Try}
 import scala.xml.Node
 
 final case class EntityPrototypeEntry(name: String,
@@ -15,14 +16,26 @@ final case class EntityPrototypeEntry(name: String,
 
 object EntityPrototypeEntry:
 
-    def fromXML(xml: Node): Option[EntityPrototypeEntry] = Try {
-        val name = (xml \ "name").text.trim
-        val availableStates = (xml \ "availableStates" \ "state").map(_.text.trim).map(State.valueOf)
-        val hasPosition = (xml \ "hasPosition").text.trim.toBoolean
-        val hasDirection = (xml \ "hasDirection").text.trim.toBoolean
-        val physicsSelectorId = (xml \ "physicsSelectorId").map(_.text.trim.toInt).headOption
-        val layer = (xml \ "layer").map(_.text.trim.toInt).headOption
-        val animationSelectorId = (xml \ "animationSelectorId").map(_.text.trim.toInt).headOption
+    def fromXML(xml: Node): Try[EntityPrototypeEntry] =
+        val name = Try((xml \ "name").map(_.text.trim).head)
+        val availableStates = Try((xml \ "availableStates" \ "state").map(_.text.trim).map(State.valueOf))
+        val hasPosition = Try((xml \ "hasPosition").map(_.text.trim).map(_.toBoolean).head)
+        val hasDirection = Try((xml \ "hasDirection").map(_.text.trim).map(_.toBoolean).head)
+        val physicsSelectorId = Try((xml \ "physicsSelectorId").map(_.text.trim).map(_.toInt).headOption)
+        val layer = Try((xml \ "layer").map(_.text.trim).map(_.toInt).headOption)
+        val animationSelectorId = Try((xml \ "animationSelectorId").map(_.text.trim).map(_.toInt).headOption)
 
-        EntityPrototypeEntry(name, availableStates, hasPosition, hasDirection, physicsSelectorId, layer, animationSelectorId)
-    }.toOption
+        {
+            for
+                name <- name
+                availableStates <- availableStates
+                hasPosition <- hasPosition
+                hasDirection <- hasDirection
+                physicsSelectorId <- physicsSelectorId
+                layer <- layer
+                animationSelectorId <- animationSelectorId
+            yield
+                EntityPrototypeEntry(name, availableStates, hasPosition, hasDirection, physicsSelectorId, layer, animationSelectorId)
+        }.recoverWith {
+            case e => Failure(new FailedToReadObject("EntityPrototypeEntry", e.getMessage))
+        }
--- src/main/scala/src/utils/TryUtils.scala
+++ src/main/scala/src/utils/TryUtils.scala
@@ -3,7 +3,8 @@ package src.utils
 import scala.util.{Failure, Success, Try}
 
 object TryUtils:
-    private def invertTry[T](seq: Seq[Try[T]]): Try[Seq[T]] =
+
+    private def _invertTry[T](seq: Seq[Try[T]]): Try[Seq[T]] =
         def inv(seq: Seq[Try[T]], agg: Seq[T]): Try[Seq[T]] = seq match
             case Success(value) +: rest => inv(rest, value +: agg)
             case Failure(exception) +: _ => Failure(exception)
@@ -11,5 +12,13 @@ object TryUtils:
 
         inv(seq, Seq.empty)
 
+    private def _invertTry[T](opt: Option[Try[T]]): Try[Option[T]] = opt match
+        case Some(Success(value)) => Success(Some(value))
+        case Some(Failure(exception)) => Failure(exception)
+        case None => Success(None)
+
     extension[T] (iterable: Iterable[Try[T]])
-        def invertTry: Try[Seq[T]] = TryUtils.invertTry(iterable.toSeq)
+        def invertTry: Try[Seq[T]] = _invertTry(iterable.toSeq)
+
+    extension[T] (option: Option[Try[T]])
+        def invertTry: Try[Option[T]] = _invertTry(option)
